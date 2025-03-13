cc0c1acb013f7405a20aed97134dbd6fd4876ae9
Collin Andrews <pizzaTrucks@outlook.com>
2025-03-13 15:45:17 -0500

    Update loadFile.py

--- loadFile.py
+++ loadFile.py
@@ -2,16 +2,30 @@ def loadSeq(path, concatSeqs):
     printData = True
     with open(path, 'r') as file:
         if concatSeqs:
-            # Load sequeces as one long string
+            # Load sequences as one long string
             loadedSeq = [file.read().replace('\n', '')]
             loadedSeqLen = len(loadedSeq[0])
             if printData:
                 print(f'Loading File: {path}\n'
                       f'     Sequence length: {loadedSeqLen:,}\n')
-            return loadedSeq, loadedSeqLen
+            return loadedSeq
         else:
-            # Load sequeces as individual strings
+            # Load sequences as individual strings
             loadedSeq = file.read().strip().split('\n')
             if printData:
                 print(f'Loading File: {path}')
             return loadedSeq
+
+
+def loadFasta(path, printData):
+    from Bio import SeqIO
+
+    proteins = SeqIO.parse(path, "fasta")
+    if printData:
+        # Parse the file and iterate over each record
+        for record in proteins:
+            print(f'ID: {record.id}\n'
+                  f'Description: {record.description}\n'
+                  f'Sequence: {record.seq}\n')
+    return proteins
+    
