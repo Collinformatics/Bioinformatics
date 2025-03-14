d00963351cd585d2515f53b0e27fbf8dcfaf9c37
Collin Andrews <pizzaTrucks@outlook.com>
2025-03-14 13:23:46 -0500

    Update PS3.py

--- PS3.py
+++ PS3.py
@@ -7,20 +7,23 @@ import sys
 pathProteins = 'ProteinsHuman.fasta.txt'
 printSeqs = True
 saveData = True
+printData = True
 
 
-def frequency(sequences, saveFreq, savePath):
+def frequency(sequences, printData, saveFreq, savePath):
+    # Evaluate a fasta file and determine the relative frequency of AAs in the sequence
+    print(f'Calculating AA Frequency:')
     aminoAcids = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
                   'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-    printData = True
-    counts = pd.DataFrame(0, index=aminoAcids, columns=['Counts'])
     vectorsRF = []
 
     # Evaluate the data
+    iteration = 0
     for ID, sequence in sequences.items():
         seqLength = len(sequence)
 
         # Count the AAs
+        counts = pd.DataFrame(0, index=aminoAcids, columns=['Counts'])
         for AA in sequence:
             if AA in aminoAcids:
                 counts.loc[AA, 'Counts'] += 1
@@ -33,26 +36,25 @@ def frequency(sequences, saveFreq, savePath):
         rfVector = f"{ID} " + " ".join(map(str, rf))
         vectorsRF.append(rfVector)
 
-
         # Add RF list to the dictionary
         sequences[ID] = {
             'sequence': sequence,
             'RF': rf
         }
+
         if printData:
-            print(f'Processed Data:\n'
-                  f'     {ID}\n'
-                  f'     {sequences[ID]}\n')
-            printData = False
+            if iteration % 100 == 0:
+                print(f'Sequence: {iteration}\n'
+                      f'     {ID}\n'
+                      f'     {rf}\n')
+            iteration += 1
 
     if saveFreq:
-        # Save vectorsRF to a text file
+        # Save vectors to a text file
         with open(savePath, 'w') as file:
             for vector in vectorsRF:
                 print(vector)
                 file.write(vector + '\n')  # Write each vector on a new line
-        print(f"\nVectors saved at:\n"
-            f"     {savePath}\n")
 
     return sequences
 
@@ -61,5 +63,5 @@ def frequency(sequences, saveFreq, savePath):
 data = loadFasta(path=pathProteins, printData=printSeqs)
 
 # Evaluate proteins
-data = frequency(sequences=data, saveFreq=saveData,
-                 savePath= pathProteins.replace('.fasta.txt', 'Freq.txt'))
+data = frequency(sequences=data, printData=printData, saveFreq=saveData,
+                 savePath= pathProteins.replace('.fasta.txt', 'FreqAA.txt'))
